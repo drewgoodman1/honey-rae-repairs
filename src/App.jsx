25c566a2322318e2ffdd53004d1fcf5942f650d7
Andrew <goodman.drew1@gmail.com>
2024-03-28 06:45:17 -0500

    improving file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import { useEffect, useState } from "react"
-import { getAllTickets } from "./services/ticketService.js"
+import { useEffect, useState } from "react";
+import { getAllTickets } from "./services/ticketService.js";
+import "./App.css";
 
-export const App = () => {
-
-    const [allTickets, setAllTickets] = useState({}) 
-
-    useEffect(() => {
-      getAllTickets().then(ticketsArray => {
-        setAllTickets(ticketsArray)
-        console.log("TICKETS SET")
-      })
-    }, []) //runs on initial render of component
-
-
-    return (
-      <>
-
-      </>
-    )
-}
+export const App = () => {};
