a189f17b5452d54352e47377d148336d592c2e03
Andrew <goodman.drew1@gmail.com>
2024-04-03 14:54:52 -0500

    working customer detail

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Employees } from "./components/employees/Employees.jsx";
 import { NavBar } from "./components/nav/NavBar.jsx";
 import { TicketList } from "./components/tickets/TicketList.jsx";
 import { Route, Routes, Outlet } from "react-router-dom";
+import { Welcome } from "./components/welcome/Welcome.jsx";
+import { CustomerDetails } from "./components/customers/CustomerDetails.jsx";
 
 export const App = () => {
   return (
@@ -17,9 +19,14 @@ export const App = () => {
           </>
         }
       >
+        <Route index element={<Welcome />} />
         <Route path="tickets" element={<TicketList />} />
-        <Route path="customers" element={<CustomerList />} />
         <Route path="employees" element={<Employees />} />
+        <Route path="customers">
+          <Route index element={<CustomerList />} />
+          <Route path=":customerId" element={<CustomerDetails />} />{" "}
+          {/* /customers/:customerId */}
+        </Route>
       </Route>
     </Routes>
   );
---
+++ src/services/customerService.js
@@ -0,0 +1,6 @@
+export const getCustomerById = (userId) => {
+    return fetch(
+        `http://localhost:8088/customers?userId=${userId}&_expand=user`
+        ).then((res) => res.json())   
+    
+}
